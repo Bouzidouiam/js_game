72b9649861651f4e8455ea25359624b25c9181c1
bouzidouiam <bouzid.ouiam.solicode@gmail.com>
2021-12-03 10:07:21 +0100

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+/*var i, num = 53;
+        var tentatives = 10;
+        for( i = 1 ; i <= 10 ;i++){
+            var X = prompt("entrez un num");
+            tentatives--;
+            if(X == num && i<=2){
+                 alert("Barvo,tu es un génie! Vous l'avez trouvé en " + i + " tentatives.");
+                 break;
+            } 
+            else if(X == num && i>2){
+                alert("Barvo! Vous l'avez trouvé en " + i + " tentatives.");
+                break;
+            } 
+            else if(X<num && X>0 && tentatives>0)
+            {
+                alert("Too small! Vous avez encore " + tentatives + " tentatives."); 
+            }
+            else if(X>num && X<101 && tentatives>0)
+            {
+                alert("Too big! Vous avez encore " + tentatives + " tentatives.");
+            }
+        }
+        if(tentatives == 0 && X!=num){
+            alert("Looser!");
+        } */
+
+        
